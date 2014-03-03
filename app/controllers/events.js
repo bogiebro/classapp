28f9608c9c1e6c06d7b93593f3519515f0285fd0
james <jamesbatchelder@optonline.net>
2014-03-02 19:52:25 -0500

    very basic events in
    Appears  good  but you cant add events I have a bug in the js it cant
    read input data

--- app/controllers/events.js
+++ app/controllers/events.js
@@ -2,6 +2,15 @@ angular.module("app.events", ['app.auth'])
 
 .controller('EventsCtrl', function ($scope, $group) {
     $scope.group = $group.props;
-    // $scope.$watch('state', function (newval, oldval) {
-    //     console.log(newval)});
+    $scope.testing  = "James";
+    $scope.eventsList = [{date:"3/2/2014", message:"Presentation prep", location:"Swing Space"},
+         {date:"3/3/2014", message:"Presentation", location:"DL220"}];
+     
+     $scope.newEvent = function(){
+
+        $scope.eventsList.push({date:$scope.dateInput, message:$scope.messageInput,
+                                   location:$scope.locationInput });
+        $scope.testing = "pushed event";
+        $scope.testing = $scope.messageInput;
+     }
 })
