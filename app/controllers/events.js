717403464774e13c6b01bc50540640b3fac6376b
Sam Anklesaria <amsay@amsay.net>
2014-03-02 20:13:21 -0500

    fixes for James

--- app/controllers/events.js
+++ app/controllers/events.js
@@ -2,14 +2,15 @@ angular.module("app.events", ['app.auth'])
 
 .controller('EventsCtrl', function ($scope, $group) {
     $scope.group = $group.props;
+    $scope.form = {}
     $scope.testing  = "James";
     $scope.eventsList = [{date:"3/2/2014", message:"Presentation prep", location:"Swing Space"},
          {date:"3/3/2014", message:"Presentation", location:"DL220"}];
      
      $scope.newEvent = function(){
 
-        $scope.eventsList.push({date:$scope.dateInput, message:$scope.messageInput,
-                                   location:$scope.locationInput });
+        $scope.eventsList.push({date:$scope.form.dateInput, message:$scope.form.messageInput,
+                                   location:$scope.form.locationInput });
         $scope.testing = "pushed event";
         $scope.testing = $scope.messageInput;
      }
