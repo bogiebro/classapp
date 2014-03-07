7a5590e3b6d56011450a50c38a6571440d8acccb
Isaac Reilly <isaac.g.reilly@gmail.com>
2014-03-06 21:57:18 -0500

    basic Members page

--- app/main/members.js
+++ app/main/members.js
@@ -1,5 +1,26 @@
 angular.module("app.members", ['app.auth'])
 
-.controller('MembersCtrl', function ($scope) {
-    // members controller goes here
+.controller('MembersCtrl', function ($scope, $group) {
+    $scope.group = $group.props;
+    $scope.group.name = "AwesomeGroup17"
+    $scope.form = {}
+    $scope.testing  = "Isaac";
+    $scope.membersList = [{name:"Jane Doe"},{name:"Yejin Xu"},{name:"Preeti Patel"}];
+
+    $scope.inviteMember = function(){
+
+        $scope.membersList.push({name:$scope.form.memberNameInput});
+        $scope.testing = "pushed event";
+        $scope.testing = $scope.messageInput;
+	$scope.form.memberNameInput = "";
+    }
+
+    $scope.inviteGroupMembers = function(){
+
+        $scope.membersList.push({name:"blah"},{name:"de"},{name:"blah"},{name:"blah..."});
+        $scope.testing = "pushed event";
+        $scope.testing = $scope.messageInput;
+	$scope.form.groupInput = "";
+    }
+
 })
