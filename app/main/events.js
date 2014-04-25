1386c580dc84500c5c49212c66683beb6e3f0002
Sam Anklesaria <amsay@amsay.net>
2014-04-24 23:48:51 -0400

    started new events idea

--- app/main/events.js
+++ app/main/events.js
@@ -1,158 +1,106 @@
-angular.module("app.events", ['app.auth'])
+angular.module("app.events", ['app.auth', 'ui.keypress'])
 
 
 .controller('EventsCtrl', function ($scope, $firebase, $group, $ref, $users, $timeout) {
-   
-//     $scope.eventList= $ref.base.child('groups/' + $group.props.id + '/events');
-//           console.log("new ref is " + $scope.eventList)
-//           var eventref= $ref.base.child('groups/' + $group.props.id + '/events');
-//            $scope.eventRef = $firebase(eventref);
-//     console.log("eventctl1")
-//     
-//     $scope.form = {}
-//     $scope.dayobject = {}
-//     $scope.today= new Date()
-//     $scope.editing = false;
-//     $scope.editID = "";
-//     
-//
-//     $scope.testing = "Space reserved for test messages";
-//
-//          $scope.testing = $scope.group.id;
-//
-//     $scope.isCollapsed = true;
-//     $scope.display = {}
-//     $scope.createButton = "Create an Event"
-//
-//
-//     $group.props.watch("classCode", function(id, oldval, newval){
-//    //     
-//          $scope.isCollapsed = true;
-//          $scope.eventList= $ref.base.child('groups/' + $group.props.id + '/events');
-//         //  console.log("new ref is " + $scope.eventList)
-//           var eventref= $ref.base.child('groups/' + $group.props.id + '/events');
-//            $scope.eventRef = $firebase(eventref);
-//      //       console.log("check")
-// //
-//        
-//     });
-//
-//
-//
-//     $scope.expandCreator = function(){
-//         $scope.isCollapsed = !$scope.isCollapsed;
-//         if( $scope.isCollapsed) {
-//             $scope.createButton = "Create an Event";
-//         }else{
-//             $scope.createButton = "close event creator";
-//         }
-//     }
-//
-//      $scope.newEvent = function(){
-//         var eventMessage = "TBA";
-//         var eventLocation = "TBA";
-//         //var eventTime = 11;
-//         if ($scope.form.messageInput ) eventMessage = $scope.form.messageInput;
-//         if ($scope.form.locationInput ) eventLocation = $scope.form.locationInput;
-//         if (parseInt($scope.form.messageInput)) eventMessage = $scope.form.messageInput;
-//       //   console.log(eventTime)
-//         //  console.log($scope.form.AM)
-//         //if (! $scope.form.AM) eventTime = eventTime +12;
-//         $scope.testing = "called";
-//       //  console.log(eventTime)
-//        // $ref.base.child("groups/" + $group.props.id ).once 'value' (snap)
-//        //$scope.dayobject.dt.setHours(eventTime);
-//        var mem = {};
-//        var name = $ref.netid;
-//        mem[name] = $ref.netid;
-//         var newevent = {"date":$scope.dayobject.dt.toUTCString(),
-//                 "message":eventMessage, "location":eventLocation, Members:mem};
-//
-//      // $ref.base.child('groups/' + $group.props.id).push(newevent);
-//
-//           $scope.eventList.push().setWithPriority(newevent,
-//                                  $scope.dayobject.dt.getTime());
-//
-//       
-//          console.log("pushed event")
-//          $scope.form.locationInput = "";
-//          $scope.form.messageInput = "";
-//      }
-//     
-//     
-//
-//      $scope.toDate = function(dateString){
-//         var newdate = new Date(dateString);
-//         
-//         return newdate;
-//      }
-//
-//     
-//      $scope.eventList.endAt($scope.today.getTime()).on('child_added', function(snapshot) {
-//         var EventInfo = snapshot.val();
-//         var eventName = snapshot.name();
-//       // $scope.eventList.child(eventName).remove();
-//         EventInfo.remove();
-//     });
-//
-//
-//      $scope.edit = function( eventId){
-//        // $scope.edtingID = eventID;
-//        // $scope.editing = true;
-//        //$scope.editRef = 
-//         console.log(eventId)
-//      }
-//
-//
-//
-//     
-//     $scope.today = function() {
-//         //sets the date to the current date
-//         $scope.dayobject.dt = new Date();
-//     };
-//     $scope.today();
-//
-//     $scope.showWeeks = true;
-//     $scope.toggleWeeks = function () {
-//      $scope.showWeeks = ! $scope.showWeeks;
-//     };
-//
-//     $scope.clear = function () {
-//      $scope.dt = null;
-//     };
-//
-//     // Disable weekend selection
-//     $scope.disabled = function(date, mode) {
-//      return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
-//     };
-//
-//     $scope.toggleMin = function() {
-//      $scope.minDate = ( $scope.minDate ) ? null : new Date();
-//     };
-//     $scope.toggleMin();
-//
-//     $scope.open = function($event) {
-//         $event.preventDefault();
-//         $event.stopPropagation();
-//
-//      $scope.opened = true;
-//     };
-//
-//     $scope.dateOptions = {
-//         'year-format': "'yy'",
-//         'starting-day': 1
-//     };
-//
-//     $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'shortDate'];
-//     $scope.format = $scope.formats[0];
-//    
-//
-//    $scope.convertTime = function (date) {
-//        var dt = new Date(date);
-//        return dt.getTime();
-//    }
-//
-//
-//
+
+  // track the current group's events
+  $scope.group = $group.props;
+  $scope.$watch('group.id', function (newvalue, oldvalue) {
+    if (newvalue) {
+      if ($scope.events) {
+        $scope.events.$off();
+      }
+      $scope.events = $firebase($ref.base.child('groups/' +
+          $group.props.id + '/events').startAt(new Date().getTime()));
+    }
+  });
+    
+  // create a new event
+  $scope.newEvent = function () {
+    var d = new Date();
+    d.setDate(d.getDate() + 1);
+    var t = d.getTime();
+    $scope.events.$add({message: 'New event', time: t}).then(function (r) {
+      $scope.mostRecent = r.name();
+      r.setPriority(t);
+    });
+  }
+
+  // get the color of the event at index i
+  var colors = ['LightBlue', 'LightCyan', 'Plum', 'LavenderBlush', '', 'rgba(245, 245, 245, 0.4)'];
+  $scope.getColor = function(i) {
+    return colors[i % colors.length];
+  };
+
+  // show a thing to join, see who else has joined. is it a spontanious group?
+  $scope.showInfo = function() {
+  };
+
+  // edit the event text/ date
+  $scope.editEvent = function(obj) {
+    $scope.mostRecent = obj.$id;
+  };
+ 
+  // close the editing field
+  $scope.closeMe = function(elem) {
+    var newtime =  Date.fromString(elem.message).getTime();
+    if (newtime > new Date().getTime()) {
+      elem.time = newtime;
+    }
+    elem.$priority = elem.time;
+    $scope.mostRecent = null;
+    $scope.events.$save(elem.$id);
+  };
 
 })
+
+.directive('syncFocusWith', function($timeout, $rootScope) {
+    var timer;
+    return {
+        restrict: 'A',
+        scope: {
+            focusValue: "=syncFocusWith"
+        },
+        link: function($scope, $element, attrs) {
+          if (timer) {
+            clearTimeout(timer);
+          }
+          timer = setTimeout(function() {
+            if ($scope.focusValue) {
+              $element[0].focus();
+            }
+          }, 0);
+          $scope.$watch("focusValue", function(currentValue, previousValue) {
+              if (currentValue === true && !previousValue) {
+                  $element[0].focus();
+              } else if (currentValue === false && previousValue) {
+                  $element[0].blur();
+              }
+          });
+        }
+    }
+})
+
+.directive('sglclick', ['$parse', function($parse) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attr) {
+          var fn = $parse(attr['sglclick']);
+          var delay = 300, clicks = 0, timer = null;
+          element.on('click', function (event) {
+            clicks++;  //count clicks
+            if(clicks === 1) {
+              timer = setTimeout(function() {
+                scope.$apply(function () {
+                    fn(scope, { $event: event });
+                }); 
+                clicks = 0;             //after action performed, reset counter
+              }, delay);
+              } else {
+                clearTimeout(timer);    //prevent single-click action
+                clicks = 0;             //after action performed, reset counter
+              }
+          });
+        }
+    };
+}])
