1514361bc2b4f6bc1ccbe7b43f62588e1771679b
Sam Anklesaria <amsay@amsay.net>
2014-04-26 13:58:21 -0400

    added bigevents

--- app/main/bigevents.js
+++ app/main/bigevents.js
@@ -1,7 +1,19 @@
-angular.module("app.bigevents", ['app.auth'])
+angular.module("app.bigevents", ['app.auth', 'firebase'])
 
-.controller('BigEventsCtrl', function ($scope, $location) {
-    $scope.goBack = function() {
-        $location.path('/')
-    }
+.controller('BigEventsCtrl', function ($scope, $ref, $firebase) {
+  $scope.names = {};
+  $scope.groupEvents = {};
+  $scope.groups = {};
+  
+  $ref.base.child('users/' + $ref.netid + '/groups').on('child_added', function (snap) {
+    var gid = snap.val();
+    $scope.groups[gid] = gid;
+    $scope.names[gid] = $firebase($ref.base.child('groups/' + gid + '/props/name'));
+    $scope.groupEvents[gid] = $firebase($ref.base.child('groups/' + gid + '/events'));
+  });
+  $ref.base.child('users/' + $ref.netid + '/groups').on('child_removed', function (snap) {
+    var gid = snap.val();
+    $scope.names[gid].$off();
+    $scope.groupEvents[gid].$off();
+  })
 })
