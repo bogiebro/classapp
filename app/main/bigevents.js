0c500ef38b67310040b0ba90c999132cda8872ae
Sam Anklesaria <amsay@amsay.net>
2014-04-27 12:49:48 -0400

    event styling changes

--- app/main/bigevents.js
+++ app/main/bigevents.js
@@ -4,17 +4,20 @@ angular.module("app.bigevents", ['app.auth', 'firebase', 'app.color'])
   $scope.names = {};
   $scope.groupEvents = {};
   $scope.groups = {};
+  $scope.numGroups = 0;
 
   // use the color scheme
   $scope.getColor = $color;
   
   $ref.base.child('users/' + $ref.netid + '/groups').on('child_added', function (snap) {
+    $scope.numGroups++;
     var gid = snap.val();
     $scope.groups[gid] = gid;
     $scope.names[gid] = $firebase($ref.base.child('groups/' + gid + '/props/name'));
     $scope.groupEvents[gid] = $firebase($ref.base.child('groups/' + gid + '/events'));
   });
   $ref.base.child('users/' + $ref.netid + '/groups').on('child_removed', function (snap) {
+    $scope.numGroups--;
     var gid = snap.val();
     $scope.names[gid].$off();
     $scope.groupEvents[gid].$off();
