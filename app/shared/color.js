b22aba62871fd5907c4c007591f5019ce160f660
Jadon Montero <jadoncm@gmail.com>
2014-04-28 04:36:22 -0400

    testing with a classy look for group colors

--- app/shared/color.js
+++ app/shared/color.js
@@ -2,7 +2,9 @@ angular.module('app.color', [])
 
 // get the color of the event at index i
 .factory('$color', function () {
-  var colors = ['LightBlue', 'LightCyan', 'Plum', 'LavenderBlush', '', 'rgba(245, 245, 245, 0.4)'];
+  //Testing with a more classy look
+  var colors = ['rgba(126,126,126, 0.8)', 'rgba(112,112,112, 0.8)'];
+  //var colors = ['LightBlue', 'LightCyan', 'Plum', 'LavenderBlush', 'rgba(112,112,112, 0.8)', 'rgba(245, 245, 245, 0.4)'];
   return function(i) {
     return colors[i % colors.length];
   };
